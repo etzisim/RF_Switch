a3707a6b87d6a9ba367c750a11d4bb580fb768f3
c-math <christer@fastmail.net>
2015-05-06 16:38:30 +0200

    Discard first pulse if low

--- RF_Switch.cpp
+++ RF_Switch.cpp
@@ -10,10 +10,15 @@ volatile uint32_t transitionTimes[BUFFERSIZE] = {0};
 volatile int buffered;
 volatile int toWrite;
 int toRead;
+bool firstRead = true;
+int pin;
 
 void clockTransition(){
-	if (buffered > 0 && toWrite == toRead){
-		return;
+	if (buffered > 0 && toWrite == toRead){return;}
+
+	if (firstRead){
+		firstRead = false;
+		if (!digitalRead(pin)){return;}
 	}
 
 	transitionTimes[toWrite] = micros();
@@ -25,6 +30,7 @@ RF_Switch::RF_Switch(int rxPin){
 	assert(rxPin == 3 || rxPin == 2);
 
 	attachInterrupt(rxPin-2, clockTransition, CHANGE);
+	pin = rxPin;
 
 	toWrite = 0;
 	toRead = 0;
@@ -34,9 +40,7 @@ RF_Switch::RF_Switch(int rxPin){
 }
 
 uint16_t RF_Switch::highPulse(){
-	if (buffered > 0 || !nextPulseHigh){
-		return 0;
-	}
+	if (buffered < 1 || !nextPulseHigh){return 0;}
 
 	toRead = next(toRead);
 	buffered--;
@@ -47,9 +51,7 @@ uint16_t RF_Switch::highPulse(){
 }
 
 uint16_t RF_Switch::lowPulse(){
-	if (buffered > 0 || nextPulseHigh){
-		return 0;
-	}
+	if (buffered < 1 || nextPulseHigh){return 0;}
 
 	toRead = next(toRead);
 	buffered--;
